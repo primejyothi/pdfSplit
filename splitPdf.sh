44629628a423edbb61c6625d7c3567e63252f8af
Prime Jyothi <primejyothi@gmail.com>
2014-01-20 10:30:17 +0530

    Making -t option mandatory

--- splitPdf.sh
+++ splitPdf.sh
@@ -3,14 +3,13 @@
 # Script to build smaller pdf files from pdf files in a given folder.
 # 
 # Extract the images from the original pdf
-# Reduce the quality of the images to 10%
+# Reduce the quality of the images if outupt quality is specified
 # Generate pdf files with 10 or 15 images.
 # Combine all the pdf files generated in the previous step
 # Requirements / Dependencies
 # 	1. Requires convert utility from ImageMagick suite
 #	2. Requires pdfunite from poppler package
-#	3. Need sufficient space in the directory pointed by imgF
-#		which points to /tmp/imgs
+#	3. Need sufficient space in the directory specified by the -t option
 # Assumptions:
 # 	1. Pdf files are generated out of images and do not contain text.
 #	2. The required data is in the extracted jpg files and not in ppm files
@@ -24,12 +23,16 @@
 
 function log ()
 {
-	echo "Log : $@"
+	lgLine=`printf "%4d" $1`
+	shift
+	echo "[Log   : ${lgLine}] $@"
 }
 
 function err ()
 {
-	echo "Error : $@"
+	lgLine=`printf "%4d" $1`
+	shift
+	echo "[Error : ${lgLine}] $@"
 }
 
 function dbg ()
@@ -38,12 +41,20 @@ function dbg ()
 	then
 		return
 	fi
-	echo "Debug : $@"
+	lgLine=`printf "%4d" $1`
+	shift
+	echo "[Debug : ${lgLine}] $@"
 }
 
 function help ()
 {
 	echo "Usage `basename $0` [-d] [-h] -c ContorlFile -f PDF Folder -p Pages per Student [-q image quality] -o OutpuFolder -t TempFolder"
+	echo -e "\t -c : Control File"
+	echo -e "\t -f : Input data folder"
+	echo -e "\t -p : Pages per student"
+	echo -e "\t -q : Image Quality"
+	echo -e "\t -o : Output Folder"
+	echo -e "\t -t : Temporary Folder"
 	echo -e "\t -d : Enable debug messages"
 	echo -e "\t -h : Display this help message"
 }
@@ -51,7 +62,7 @@ function help ()
 function createPdfs ()
 {
 	del="$*"
-	log $LINENO  "Converting images to PDFs"
+	log $LINENO  "Converting images into PDFs"
 	pdfPartName=`basename "$currentPDF" .pdf`
 
 	# newName=${schoolCode}_${pdfPartName}_${startPage}_${currentPage}
@@ -60,7 +71,7 @@ function createPdfs ()
 	pgNo=`printf %04d $currentPage`
 	newName=${schoolCode}_${pdfPartName}_${pgNo}
 
-	dbg "$LINENO New name [$newName]"
+	dbg $LINENO "New name [$newName]"
 	convert $del ${imgPDF}/"${newName}".pdf
 	dbg $LINENO  "Deleting [$del]"
 	rm $del
@@ -73,31 +84,31 @@ function splitPdf ()
 	if [[ -z "$pdf2Split" ]]
 	then
 		# No more PDF files to split, exit
-		log "$LINENO No more PDF files to split, exiting"
+		log $LINENO "No more PDF files to split, exiting"
 		exit 0
 	fi
-	log "$LINENO  Splitting pdf file [$pdf2Split] into images."
-	dbg $LINENO  splitPdf $num
+	log $LINENO  "Splitting pdf file [$pdf2Split] into images."
+	dbg $LINENO  "splitPdf $num"
 	pdfimages -p -j "$pdf2Split" $imgF/jj
 	rm $imgF/*.ppm
 	if [[ ! -z "${imgQlty}" ]]
 	then
-		log $LINENO Image Quality $imgQlty
+		log $LINENO "Output image quality set to ${imgQlty}%"
 		convertImages 
 	else
-		log $LINENO Image Quality Empty
+		log $LINENO "Image Quality Empty"
 	fi
 	currentPDF=$pdf2Split
 }
 
 function convertImages ()
 {
-	log "$LINENO Changing the image quality to [${imgQlty}%]"
+	log $LINENO "Changing the image quality to [${imgQlty}%]"
 	ls ${imgF}/*.jpg > conv.lst 2> /dev/null
 	
 	while read conv
 	do
-		dbg $LINENO  [$conv]
+		dbg $LINENO  "[$conv]"
 		convert -quality ${imgQlty} "$conv" ${imgF}/new.jpg
 		# Rename the converted file.
 		mv ${imgF}/new.jpg "$conv"
@@ -113,17 +124,17 @@ function combinePdfs ()
 		return
 
 	fi
-	log "$LINENO $ucount Combining the PDFs"
+	log $LINENO "Combining [$ucount] PDFs"
 	ls  ${imgPDF}/*.pdf > ls.lst
 	while read i 
 	do
-		dbg "$LINENO $i"
+		dbg $LINENO "$i"
 	done <  ls.lst
 
 	pgNo=`printf %04d $currentPage`
 	pdfPartName=`basename "$currentPDF" .pdf`
 	newPDFName=${schoolCode}_${pdfPartName}_${startPage}_${pgNo}
-	log $LINENO new name [$newPDFName]
+	log $LINENO "New name [$newPDFName]"
 
 	if [[ "$ucount" -eq 1 ]]
 	then
@@ -232,7 +243,7 @@ do
 	if [[ "${ret}" -ne 0 ]]
 	then
 		# Expr failed, mostly due to invalid student count
-		err "Error while processing school : ${schoolCode}"
+		err $LINENO "Error while processing school : ${schoolCode}"
 		errors="Yes"
 		continue
 	fi
@@ -253,7 +264,7 @@ fi
 for outDirs in $imgPDF $imgF $outPDF
 do
 	outFileCount=`ls ${outDirs}/* 2> /dev/null | wc -l`
-	dbg $LINENO  outFileCount $outFileCount
+	dbg $LINENO  "outFileCount $outFileCount"
 
 	if [[ "$outFileCount" -gt 0 ]]
 	then
@@ -265,7 +276,7 @@ done
 # Check for the output directory.
 if [[ ! -d "$outPDF" ]]
 then
-	err "$LINENO : Unable to access output directory $outDir"
+	err $LINENO "Unable to access output directory $outPDF"
 	exit 2
 fi
 
@@ -278,23 +289,29 @@ fi
 log $LINENO  "Totals schools [$totalSchools], students [${totalStudents}] pages [${totalPages}]"
 log $LINENO  "Control file validation completed."
 
+# 
 # Check for the temporary directories
 if [[ -d "$tDir" ]]
 then
 	mkdir ${imgF} ${imgPDF} 2> /dev/null
+else
+	log $LINENO "Temp directory ${tDir} not accessible"
+	log $LINENO "Try setting -t "
+	help
+	exit 2
 fi
 
 for outDirs in ${imgF} ${imgPDF}
 do
 	if [[ ! -d ${outDirs} ]]
 	then
-		err "$LINENO Unable to access temp directory ${outDirs}"
+		err $LINENO "Unable to access temp directory ${outDirs}"
 		errors="Yes"
 	fi
 done
 if [[ "$errors" = "Yes" ]]
 then
-	err "$LINENO Unable to access temp directory"
+	err $LINENO "Unable to access temp directory"
 	exit 2
 fi
 
@@ -326,29 +343,29 @@ do
 	do
 		# Calculate the fetch size.
 		rem=`expr $pageReq \- $pagesProcessed`	
-		# dbg $LINENO  rem $rem
+		# dbg $LINENO  "rem $rem"
 		if [ $rem -lt $batchSize -a $rem -gt 0 ]
 		then
-			dbg "$LINENO Changing fetch size to $rem"
+			dbg $LINENO "Changing fetch size to $rem"
 			fetchSize=$rem
 		else
 			fetchSize=$batchSize
 		fi
-		dbg $LINENO : fetchSize $fetchSize
+		dbg $LINENO "fetchSize $fetchSize"
 
 		imgRes=`getImages $fetchSize`
 		imgCount=`echo $imgRes | cut -d"|" -f 1`
 		images=`echo $imgRes | cut -d"|" -f 2`
-		dbg $LINENO  imageCount [$imgCount]
-		# dbg $LINENO  images $images
+		dbg $LINENO  "imageCount [$imgCount]"
+		# dbg $LINENO  "images $images"
 
 		if [[ $imgCount -eq 0 ]]
 		then
 			# About to start processing new PDF file. Combine the smaller
 			# PDFs generated from the previous file.
-			log $LINENO  Calling combinePdfs
+			dbg $LINENO  "Calling combinePdfs"
 			combinePdfs
-			log $LINENO  "Splitting new PDF into images"
+			dbg $LINENO  "Splitting new PDF into images"
 			splitPdf $pdfSeq
 			pdfSeq=`expr $pdfSeq + 1`
 			currentPage=0
@@ -358,22 +375,22 @@ do
 
 		pagesProcessed=`expr $pagesProcessed + $imgCount`
 		currentPage=`expr $currentPage + $imgCount`
-		log $LINENO Pages processed : $pagesProcessed
+		log $LINENO "Pages processed for school [$schoolCode]: $pagesProcessed"
 
 		# convert to mini pdfs
 		log $LINENO "Current file [$currentPDF] start page [$startPage] end Page :[$currentPage]"
-		log "$LINENO Converting images into PDF."
+		dbg $LINENO "Converting images into PDF."
 		createPdfs $images 
 
 		if [[ $pagesProcessed -ge $pageReq ]]
 		then
-			log "$LINENO pages processed = $pagesProcessed, breaking"
+			log $LINENO "Pages processed for school [$schoolCode] = $pagesProcessed, breaking"
 			combinePdfs
 			break
 		fi
 
 	done
-	log "$LINENO Pages Processed $pagesProcessed"
+	log $LINENO "Total pages processed for school [$schoolCode] : $pagesProcessed"
 
 
 done < ${ctrlFile}
